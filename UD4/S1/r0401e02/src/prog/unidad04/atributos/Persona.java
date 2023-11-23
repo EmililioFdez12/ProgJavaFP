aadd4a3bf7eeb66953c560167d9a3cee96ab11fb
EmilioFdez12 <emiliofdezglld12@gmail.com>
2023-11-23 13:43:28 +0100

    ej personas

---
+++ UD4/S1/r0401e02/src/prog/unidad04/atributos/Persona.java
@@ -0,0 +1,19 @@
+package prog.unidad04.atributos;
+
+public class Persona {
+  //Sexo
+  
+  
+  public String sexo;
+     
+  public String nombre;
+  
+  public int edad;
+  
+  public String dni;
+  
+  public double altura;
+  
+  public double peso;
+
+}
