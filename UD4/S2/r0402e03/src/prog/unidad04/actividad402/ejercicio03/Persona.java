645d13072f96a9a0d1f22e08741a2c8264473ef4
EmilioFdez12 <emiliofdezglld12@gmail.com>
2023-11-27 14:18:28 +0100

    Clases private

---
+++ UD4/S2/r0402e03/src/prog/unidad04/actividad402/ejercicio03/Persona.java
@@ -0,0 +1,33 @@
+package prog.unidad04.actividad402.ejercicio03;
+
+public class Persona {
+
+  private String nombre;
+  private int edad;
+  private char sexo;
+  private double estatura;
+  private double peso;
+  private String obtenerCadena;
+
+  public Persona() {
+
+  }
+
+  public Persona(String nombre, int edad, char sexo) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.sexo = sexo;
+  }
+
+  public Persona(String nombre, int edad, char sexo, double estatura, double peso) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.sexo = sexo;
+    this.estatura = estatura;
+    this.peso = peso;
+
+  }
+
+  public String obtenerCadena() {
+    
+}
