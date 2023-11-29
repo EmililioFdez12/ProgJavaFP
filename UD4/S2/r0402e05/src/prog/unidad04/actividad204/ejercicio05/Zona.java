793626aa05a6eb3fa535c43061cc75b57e4ad5e3
EmilioFdez12 <emiliofdezglld12@gmail.com>
2023-11-29 20:39:39 +0100

    Ejercicio geometria DONE

--- UD4/S2/r0402e05/src/prog/unidad04/actividad204/ejercicio05/Zona.java
+++ UD4/S2/r0402e05/src/prog/unidad04/actividad204/ejercicio05/Zona.java
@@ -1,23 +1,32 @@
 package prog.unidad04.actividad204.ejercicio05;
 
+import java.util.Scanner;
+
 public class Zona {
-  
-  private int entradas;
-  
-  public Zona(int entradas) {
-    this.entradas = entradas;
-  }
-  
-  public int getEntradasPorVender() {
-    return entradas;
-  }
-  
-  public int vender() {
-       
-    
-    return entradas;
-  }
-  
-  
+	Scanner sc = new Scanner(System.in);
+
+	private int entradasIniciales;
+
+	public Zona(int entradasIniciales) {
+		this.entradasIniciales = entradasIniciales;
+	}
+
+	public int getEntradasPorVender() {
+		return entradasIniciales;
+	}
+
+	public int vender() {
+		System.out.print("Introduzca el numero de asientos a vender: ");
+		int entradasAVender = Integer.parseInt(sc.nextLine());
+
+		if (entradasIniciales < entradasAVender) {
+			System.out.print("El numero de entradas a vender es mayor de las entradas que disponemos");
+		} else {
+			System.out.println("Venta realizada correctamente");
+			entradasIniciales -= entradasAVender;
+		}
+
+		return entradasIniciales;
+	}
 
 }
