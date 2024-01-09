f357c37356c014a47851f83513c742bf57883a18
EmililioFdez12 <emiliofdezglld12@gmail.com>
2024-01-09 14:42:21 +0100

    Unidad 06 añadida

---
+++ UD6/Actividad601(VI)/src/prog/unidad06/arraysuni/ejercicio06/EstadisticasNotas.java
@@ -0,0 +1,23 @@
+package prog.unidad06.arraysuni.ejercicio06;
+
+public class EstadisticasNotas {
+
+  public EstadisticasNotas() {
+    
+  }
+  
+  public void addCalification(int calificacion) {
+    
+  }
+  
+  public double calificacionMedia() {
+    return 0;
+    
+  }
+  
+  public double calificacionModa() {
+    return 0;
+    
+  }
+  
+}
---
+++ UD6/Actividad601(VII)/src/prog/unidad06/arraysuni/ejercicio07/SuperArrays.java
@@ -0,0 +1,14 @@
+package prog.unidad06.arraysuni.ejercicio07;
+
+import java.util.Scanner;
+
+public class SuperArrays {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Introduce un número entero cualquiera");
+
+  }
+
+}
