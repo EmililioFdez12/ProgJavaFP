74fb568a710986c81a4cf771733816b7ee57abd9
EmililioFdez12 <emiliofdezglld12@gmail.com>
2024-01-10 14:40:52 +0100

    Ej 7

--- UD6/Actividad601(VI)/src/prog/unidad06/arraysuni/ejercicio06/EstadisticasNotas.java
+++ UD6/Actividad601(VI)/src/prog/unidad06/arraysuni/ejercicio06/EstadisticasNotas.java
@@ -2,22 +2,49 @@ package prog.unidad06.arraysuni.ejercicio06;
 
 public class EstadisticasNotas {
 
+  private int[] arrayCalificaciones;
+  private int cantidadCalificaciones = 0;
+
   public EstadisticasNotas() {
-    
+    this.arrayCalificaciones = new int[20];
+    this.cantidadCalificaciones = 0;
   }
-  
-  public void addCalification(int calificacion) {
-    
+
+  public void addCalificacion(int calificacion) {
+
+    if (calificacion < 0 || calificacion > 10) {
+      throw new IllegalArgumentException();
+    } else {
+      if (cantidadCalificaciones < arrayCalificaciones.length) {
+        arrayCalificaciones[cantidadCalificaciones] = calificacion;
+        cantidadCalificaciones++;
+      } else {
+        System.out.println("No hay más espacio disponible");
+      }
+    }
   }
-  
+
   public double calificacionMedia() {
-    return 0;
+    double sumaNotas = 0;
+
+    if (cantidadCalificaciones < 1) {
+      return 0;
+    }
+
+    for (int i = 0; i < cantidadCalificaciones; i++) {
+      sumaNotas += arrayCalificaciones[i];
+    }
+    double media = sumaNotas / cantidadCalificaciones;
+    return media;
+  }
+
+  public double calificacionModa() throws EstadisticasNotasException {
+    
+    if(cantidadCalificaciones < 1) {
+      throw new EstadisticasNotasException();
+    }
+    return cantidadCalificaciones;
     
   }
-  
-  public double calificacionModa() {
-    return 0;
-    
-  }
-  
+
 }
