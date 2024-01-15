0def62192455f719849302dd5a04ab783f4b89c4
EmilioFdez12 <emiliofdezglld12@gmail.com>
2024-01-15 23:25:02 +0100

    Ejercicio 5

--- UD6/S2/Actividad602/src/prog/unidad06/arraysmulti/ejercicio5/MaximoMinimoTablaAleatoriaApp.java
+++ UD6/S2/Actividad602/src/prog/unidad06/arraysmulti/ejercicio5/MaximoMinimoTablaAleatoriaApp.java
@@ -21,13 +21,45 @@ public class MaximoMinimoTablaAleatoriaApp {
         tablaAleatoria.setCasilla(i, j, generadorRandom);
       }
     }   
-
+    
+    System.out.println("MÁXIMO Y MÍNIMO EN TABLA ALEATORIA");
+    System.out.println("---------------------------------------------------------------------------");
     for (int i = 0; i < filas; i++) {
       for (int j = 0; j < columnas; j++) {
         System.out.print(tablaAleatoria.getCasilla(i, j) + "\t");
       }
       System.out.println();
     }
+    System.out.println("---------------------------------------------------------------------------");
+    
+    //El primer valor es el máximo(no hay mas)
+    int valorMaximo = tablaAleatoria.getCasilla(0, 0);
+    int valorMinimo = tablaAleatoria.getCasilla(0, 0);
+    int posicionFilaMaximo = 0;
+    int posicionColumnaMaximo = 0;
+    int posicionFilaMinimo = 0;
+    int posicionColumnaMinimo= 0;
+    
+    for(int i = 0; i < filas; i++) {
+    	for(int j = 0; j < columnas; j++) {
+    		int valorActual = tablaAleatoria.getCasilla(i, j);
+    		
+    		if (valorMaximo < valorActual) {
+    			posicionFilaMaximo = i + 1 ;// + 1 para que empiece por la fila 0
+      		posicionColumnaMaximo = j + 1;
+    			valorMaximo = valorActual;
+    		} 
+    		
+    		if (valorMinimo > valorActual) {
+    			posicionFilaMinimo = i + 1 ;// + 1 para que empiece por la fila 0
+      		posicionColumnaMinimo = j + 1;
+    			valorMinimo = valorActual;
+    		} 
+    		
+    	}
+    }
+    System.out.println("El máximo vale " + valorMaximo + " y esta locacalizado en la fila " + (posicionFilaMaximo) + " y la columna " + (posicionColumnaMaximo));
+    System.out.println("El mínimo vale " + valorMinimo+ " y esta locacalizado en la fila " + (posicionFilaMinimo) + " y la columna " + (posicionColumnaMinimo));
   }
 
 
