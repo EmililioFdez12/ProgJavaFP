1007cd1f269a9bcff6b7695598c3d457712e4dd2
EmilioFdez12 <emiliofdezglld12@gmail.com>
2024-01-16 21:53:13 +0100

    Quedan 2 ejercicios

--- UD6/S2/Actividad602/src/prog/unidad06/arraysmulti/ejercicio07/DiagonalTabla.java
+++ UD6/S2/Actividad602/src/prog/unidad06/arraysmulti/ejercicio07/DiagonalTabla.java
@@ -4,31 +4,35 @@ import prog.unidad06.arraysmulti.ejercicio06.TablaEnteraUtils;
 
 public class DiagonalTabla {
 
-  public static void main(String[] args) {
-    int[][] tabla = TablaEnteraUtils.nuevaTablaAleatoria(10, 10, 200, 300);
+	public static void main(String[] args) {
 
-    TablaEnteraUtils.imprimeTabla(tabla, 2);
+		int[][] tabla = TablaEnteraUtils.nuevaTablaAleatoria(10, 10, 200, 300);
+		int minimo = tabla[0][0];
+		int maximo = tabla[0][0];
+		double suma = tabla[0][0];
 
-    System.out.print("Los elementos que están en la diagonal son: ");
+		TablaEnteraUtils.imprimeTabla(tabla, 2);
 
-    int[][] arrayDiagonal = new int[10][10];
-    
-    for (int i = 0; i < 10; i++) {
-      // Imprimir elemento diagonal
-      System.out.print(tabla[i][i]);
-      arrayDiagonal[i][i] = tabla[i][i];
-      // Comprobar si es el último elemento de la diagonal
-      if (i < 9) {
-        System.out.print(", ");
-      }
-    }
-    System.out.println();
-    
-    int sumaValores = 0;
-        
-    
-    System.out.println("El máximo de los elementos de la diagonal es " + TablaEnteraUtils.getMaximo(arrayDiagonal)
-    + " el mínimo es " + TablaEnteraUtils.getMinimo(arrayDiagonal) + " y la media de todos los elementos de la diagonal es "
-    + "");
-  }
+		System.out.print("Los elementos que están en la diagonal son: ");
+
+		int[][] arrayDiagonal = new int[10][10];
+
+		for (int i = 0; i < 10; i++) {
+			// Imprimir elemento diagonal
+			System.out.print(", " + tabla[i][i]);
+			arrayDiagonal[i][i] = tabla[i][i];
+
+			if (tabla[i][i] < minimo) {
+				// Ahora es el nuevo mínimo
+				minimo = tabla[i][i];
+			}
+			// Añadimos a la suma de la media
+			suma += tabla[i][i];
+		}
+
+		System.out.println();
+
+		System.out.println("El máximo de los elementos de la diagonal es " + maximo + " el mínimo es " + minimo
+				+ " y la media de todos los elementos de la diagonal es " + (suma / 10));
+	}
 }
