160b1523d6a99d1d140e5b9bf3680ce325d8c055
EmililioFdez12 <emiliofdezglld12@gmail.com>
2024-03-11 14:42:19 +0100

    Rel 505 casi terminada

---
+++ UD5/Actividad505/src/prog/unidad05/gui/ejercicio08/EstadisticasSwingApp.java
@@ -0,0 +1,43 @@
+package prog.unidad05.gui.ejercicio08;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class EstadisticasSwingApp {
+
+  private JFrame frame;
+
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          EstadisticasSwingApp window = new EstadisticasSwingApp();
+          window.frame.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
+
+  /**
+   * Create the application.
+   */
+  public EstadisticasSwingApp() {
+    initialize();
+  }
+
+  /**
+   * Initialize the contents of the frame.
+   */
+  private void initialize() {
+    frame = new JFrame();
+    frame.setBounds(100, 100, 450, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+
+}
